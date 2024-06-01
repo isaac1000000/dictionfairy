22fa91e980cf410b4ef92d3f0953befaccde75cd
Isaac Fisher <47131323+Ikers00@users.noreply.github.com>
2024-05-31 20:11:55 -0400

    Added WordReference compatibility
    Still some minor issues but at least one functional response

--- webscraping/Webscraper.py
+++ webscraping/Webscraper.py
@@ -13,7 +13,8 @@ class Webscraper():
 		self.driver = webdriver.Chrome(options=options)
 		self.dictionaries = {
 			"dwds.de: de-de": ["https://dwds.de/wb/", self.parse_dwds_response],
-			"leo: de-en": ["https://dict.leo.org/german-english/", self.parse_leo_response]
+			"leo: de-en": ["https://dict.leo.org/german-english/", self.parse_leo_response],
+			"WordReference: fr-en": ["https://wordreference.com/fren/", self.parse_wr_response]
 		}
 		self.dictionary = dictionary
 
@@ -22,13 +23,23 @@ class Webscraper():
 		return self.dictionaries[self.dictionary][1]()
 
 	def parse_dwds_response(self):
+		# Parses a response from dwds.de german to german dictionary
 		result = [x.text for x in self.driver.find_elements(By.CLASS_NAME, "dwdswb-lesart-def")]
 		if not result:
 			result = ["No results"]
 		return result
 
 	def parse_leo_response(self):
+		# Parses a response from leo german to english dictionary
 		result = [x.text for x in self.driver.find_elements(By.XPATH, "//td[@lang='en']")]
+		if not result:
+			result = ["No results"]
+		return result
+
+	def parse_wr_response(self):
+		# Parses a response from WordReference french to english dictionary
+		# TODO: Still returns "Anglais" header
+		result = [x.text for x in self.driver.find_elements(By.CLASS_NAME, "ToWrd")]
 		if not result:
 			result = ["No results"]
 		return result
@@ -40,3 +51,5 @@ if __name__ == "__main__":
 	webscraper = Webscraper("dwds")
 	print(webscraper.search_dict_for("Gut"))
 	print(webscraper.search_dict_for("Prolet"))
+
+#problème
