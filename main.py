3b022f1b46b08c2dd8b1a5f280316ce61fde9483
Isaac Fisher <isaac.fisher@nyu.edu>
2024-06-02 21:45:34 -0400

    Icon from .png to .ico

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setWindowTitle("dictionfairy")
-		self.setWindowIcon(QIcon("imgs/df-icon-transparent.png"))
+		self.setWindowIcon(QIcon("imgs/df-icon-transparent.ico"))
 		self.window_size = [config["window-size"][0], config["window-size"][1]]
 		if not (self.MIN_WIDTH <= self.window_size[0] <= self.MAX_WIDTH and self.MIN_HEIGHT <= self.window_size[1] <= self.MAX_HEIGHT):
 			raise ConfigErrorException("window-size", "window width must be between 200 and 1920, and height must be between 300 and 1080")
